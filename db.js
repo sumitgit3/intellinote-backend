4c40720a3ea1ad831b03dfe62dbb3e3ba98b3332
sumitgit3 <sumitkumar703011@gmail.com>
2024-06-17 19:23:12 +0530

    "changed database to local"

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 import { connect } from 'mongoose';
-import dotenv from 'dotenv'
-dotenv.config();
-const mongoURI = process.env.MONGO_URI;
+//import dotenv from 'dotenv'
+//dotenv.config();
 
+//const mongoURI = process.env.MONGO_URI;
+const mongoURI = 'mongodb://127.0.0.1:27017/';
 const connectToMongo =  ()=>{
   connect(mongoURI)
  .then((res)=>{console.log("connected")})
