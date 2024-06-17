c8763e88e4263db986dbd098c445ef71842d29cc
sumitgit3 <sumitkumar703011@gmail.com>
2024-06-17 16:35:18 +0530

    "setup express and mongoDB atlas using mongoose"

---
+++ db.js
@@ -0,0 +1,12 @@
+import { connect } from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
+const mongoURI = process.env.MONGO_URI;
+
+const connectToMongo =  ()=>{
+  connect(mongoURI)
+ .then((res)=>{console.log("connected")})
+ .catch((err)=>{console.log(err)});
+}
+
+export default connectToMongo;
---
+++ index.js
@@ -0,0 +1,17 @@
+import connectToMongo from './db.js';
+import express from 'express';
+
+connectToMongo();
+
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
