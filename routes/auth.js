e4b4e1c746a0cb884a8014cf629aaf7163e9a296
sumitgit3 <sumitkumar703011@gmail.com>
2024-06-20 13:42:49 +0530

    Added validator using express-validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,30 @@
 /* express.Router
 Use the express.Router class to create modular, mountable route handlers. A Router instance is a complete middleware and routing system; for this reason, it is often referred to as a “mini-app”. */
 import express from 'express'
-//import model of user
-import User from '../models/Users.js'   
+import User from '../models/Users.js'
+import { body, validationResult } from 'express-validator';
+
 
 const router = express.Router();
 //create a User using POST "api/auth": doesn't require authentication
-router.post('/',(req,res)=>{
-    const user = new User(req.body);
-    user.save();
-    res.send("Welcome to authentication")
+router.post('/', [
+    body('name', "Enter a valid Name").isLength({ min: 5, max: 20 }),
+    body('email', "Enter a valid Email").isEmail(),
+    body('password', "Enter a password of length at least 8").isLength({ min: 8 })
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+    }
+    const {name,email,password} = req.body;
+    User.create({
+        name,
+        email,
+        password:password
+    }).then(user => res.json(user))
+        .catch(err => {
+            res.json({ error: "Please enter a unique email" })
+        });
 });
 
 export default router;
