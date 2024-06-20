07a1cc3e6ce76ed8f624b3d2a031fb5be01a1b9c
sumitgit3 <sumitkumar703011@gmail.com>
2024-06-20 14:26:55 +0530

    changed endpoint to /api/auth/createuser ,added a validator for email, enclosed in try catch block for error handling

--- routes/auth.js
+++ routes/auth.js
@@ -6,25 +6,38 @@ import { body, validationResult } from 'express-validator';
 
 
 const router = express.Router();
-//create a User using POST "api/auth": doesn't require authentication
-router.post('/', [
+//create a User using POST "api/auth/createuser": doesn't require login
+router.post('/createuser', [
     body('name', "Enter a valid Name").isLength({ min: 5, max: 20 }),
     body('email', "Enter a valid Email").isEmail(),
     body('password', "Enter a password of length at least 8").isLength({ min: 8 })
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         res.status(400).json({ errors: errors.array() });
     }
-    const {name,email,password} = req.body;
-    User.create({
-        name,
-        email,
-        password:password
-    }).then(user => res.json(user))
-        .catch(err => {
-            res.json({ error: "Please enter a unique email" })
-        });
+    else {
+        try {
+            const { name, email, password } = req.body;
+            //check if email already exist
+            const userExist = await User.findOne({ "email": email });
+            if (userExist) {
+                return res.status(400).send("Please try a different email. This email already exist");
+            }
+            //User.create return a promise, we are waiting for promise to be resolved
+            const user = await User.create({
+                name,
+                email,
+                password: password
+            });
+            res.json(user);
+        }
+        catch (err) {
+            console.log(err.message);
+            res.status(500).send("Some error occured");
+        }
+    }
+
 });
 
 export default router;
