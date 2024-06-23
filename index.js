617094de4d64e4869964e8706ca65c9e00edcce6
sumitgit3 <sumitkumar703011@gmail.com>
2024-06-23 14:56:16 +0530

    added cors middleware

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express';
 //import route handler
 import authRouter from './routes/auth.js'
 import notesRouter from './routes/notes.js'
+import cors from 'cors';
 //connect to dataBase
 connectToMongo();
 
@@ -11,6 +12,8 @@ const app = express()
 const port = 5000
 //use req-->use middleware to make it happen
 app.use(express.json());
+//use cors middleware
+app.use(cors());
 //available routes
 app.use('/api/auth',authRouter);
 app.use('/api/notes',notesRouter);
